9146547da8eb99946698d1899d5d5650ceb066f2
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2020-01-22 10:48:51 -0800

    solved lab 12

--- src/main/java/com/micahthor/songr/MainController.java
+++ src/main/java/com/micahthor/songr/MainController.java
@@ -1,31 +1,41 @@
 package com.micahthor.songr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String splashPage() {
-        return "index";
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @PostMapping("/albums")
+    public RedirectView postAlbums(String title, String artist, int songCount, int length, String imageURLString) {
+        Album newAlbum = new Album(title, artist, songCount, length, imageURLString);
+        albumRepository.save(newAlbum);
+        return new RedirectView("/albums");
     }
 
     @GetMapping("/albums")
     public String albums(Model m) {
 
-        Album[] albumList = new Album[3];
-        albumList[0] = new Album("Stadium Arcadium", "Red Hot Chili Peppers", 28, 7347, "https://upload.wikimedia.org/wikipedia/en/e/e6/Stadiumarcadium.jpg");
-        albumList[1] = new Album("The Division Bell", "Pink Floyd", 11, 3983, "https://upload.wikimedia.org/wikipedia/en/6/6e/Pink_Floyd_-_Division_Bell.jpg");
-        albumList[2] = new Album("El Camino", "The Black Keys", 11, 2289, "https://upload.wikimedia.org/wikipedia/en/9/92/The_Black_Keys_El_Camino_Album_Cover.jpg");
-
-        m.addAttribute(albumList);
+        List<Album> albums = albumRepository.findAll();
+        m.addAttribute("albumList", albums);
 
         return "albums";
+    }
+
+    @GetMapping("/")
+    public String splashPage() {
+        return "index";
     }
 
     @GetMapping("/hello")
